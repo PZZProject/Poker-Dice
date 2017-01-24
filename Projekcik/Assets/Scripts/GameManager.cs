13e493c47b4124a19a48566573312f3be4d5aaa5
MaciejBartczak <maciej.bartczak7@gmail.com>
2017-01-24 10:29:58 +0100

    Add files via upload

---
+++ Projekcik/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class GameManager : MonoBehaviour {
+    public List<GameObject> die;
+    public List<Text> dieValues;
+    public GameObject btn;
+    public Text text;
+    public int wynik  { get; set; }
+    public int currentValue { get; set; }
+    void Start()
+    {
+        wynik = 0;
+    }
+
+    void Update()
+    {
+        foreach (var item in die)
+        {
+            wynik += item.GetComponent<DisplayCurrentDieValue>().currentValue;
+        }
+        text.text = "Score:\n" + wynik;
+        print(wynik);
+        wynik = 0;
+
+        ///TODO
+        /*foreach (var item in dieValues)     
+        {
+            item.text = "Die " + (dieValues.IndexOf(item) + 1) + " = " + die[dieValues.IndexOf(item)].GetComponent<DisplayCurrentDieValue>().currentValue;
+        }*/
+        foreach (var item in dieValues)
+        {
+            item.text = "Die " + (dieValues.IndexOf(item) + 1) + " = " + die[dieValues.IndexOf(item)].GetComponent<DisplayCurrentDieValue>().currentValue;
+        }
+    }   
+
+
+}
---
+++ Projekcik/Assets/Scripts/Gravity.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        GetComponent<Rigidbody>().AddForce(Physics.gravity * GetComponent<Rigidbody>().mass);
+	}
+}
